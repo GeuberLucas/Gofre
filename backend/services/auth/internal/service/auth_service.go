e2a5da718853e563cf360c1ea24632477855e14d
geuber lopes <geuberlu@gmail.com>
2025-11-10 22:06:04 -0300

    feat: enhance authService with database connection handling for Login, Register, Profile, ForgotPassword, and ResetPassword methods

--- backend/services/auth/internal/service/auth_service.go
+++ backend/services/auth/internal/service/auth_service.go
@@ -1,6 +1,9 @@
 package service
 
-import dtos "github.com/GeuberLucas/Gofre/backend/services/auth/internal/DTOs"
+import (
+	"github.com/GeuberLucas/Gofre/backend/pkg"
+	dtos "github.com/GeuberLucas/Gofre/backend/services/auth/internal/DTOs"
+)
 
 
 type authService struct{}
@@ -10,5 +13,48 @@ func NewAuthService() *authService {
 }
 
 func (s *authService) Login(obj dtos.LoginDTO) (*dtos.LoginResultDto, error, string) {
+	dbConn,error := pkg.ConnectToDatabase()
+	if error != nil {
+		return nil, error, "Internal"
+	}
+	defer pkg.CloseDatabaseConnection(dbConn)
 	return nil, nil, ""
 }
+
+func (s *authService) Register(obj dtos.RegisterDTO) (*dtos.LoginResultDto, error, string) {
+	dbConn,err := pkg.ConnectToDatabase()
+	if err != nil {
+		return nil, err, "Internal"
+	}
+	defer pkg.CloseDatabaseConnection(dbConn)
+	return nil, nil, ""
+}
+func (s *authService) Profile(userID string) (*dtos.ProfileDto, error, string) {
+	dbConn,err := pkg.ConnectToDatabase()
+	if err != nil {
+		return nil, err, "Internal"
+	}
+	defer pkg.CloseDatabaseConnection(dbConn)
+	return nil, nil, ""
+}
+
+func (s *authService) ForgotPassword(email string) error {
+	dbConn,err := pkg.ConnectToDatabase()
+	if err != nil {
+		return  err
+	}
+	defer pkg.CloseDatabaseConnection(dbConn)
+	return nil
+}
+
+func (s *authService) ResetPassword(token string, newPassword string) error {
+	dbConn,err := pkg.ConnectToDatabase()
+	if err!= nil {
+		return  err
+	}
+	defer pkg.CloseDatabaseConnection(dbConn)
+	return nil
+}
+
+
+
