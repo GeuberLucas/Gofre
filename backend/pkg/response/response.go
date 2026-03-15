9cc89c5c2eb16c39a44b919e83fb70990f68d4a7
geuber lopes <geuberlu@gmail.com>
2026-03-15 11:10:01 -0300

    fix: replace fatal log with a warning for JSON response encoding errors

--- backend/pkg/response/response.go
+++ backend/pkg/response/response.go
@@ -12,7 +12,7 @@ func JSONResponse(w http.ResponseWriter, statusCode int, data interface{}) {
 	w.WriteHeader(statusCode)
 
 	if erro := json.NewEncoder(w).Encode(data); erro != nil {
-		log.Fatal(erro)
+		log.Printf("Erro ao escrever JSON de resposta: %v\n", erro)
 	}
 }
 
